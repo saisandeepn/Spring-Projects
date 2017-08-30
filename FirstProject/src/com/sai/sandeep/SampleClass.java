17acdfa43cabb45e02db4144f41db4084674ed6e
Sai Sandeep Nandyala <saisandeep99@gmail.com>
2017-08-30 16:16:07 -0700

    deleted First Project

--- FirstProject/src/com/sai/sandeep/SampleClass.java
+++
@@ -1,9 +0,0 @@
-package com.sai.sandeep;
-
-public class SampleClass {
-public static void main(String[] args) {
-	System.out.println("First file to GIT");
-	System.out.println("second commit file to GIT");
-
-}
-}

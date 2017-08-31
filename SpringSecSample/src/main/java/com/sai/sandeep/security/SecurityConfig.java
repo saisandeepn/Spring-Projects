b341f65c1d17da52d1fe1e622367a8cc0f832285
Sai Sandeep Nandyala <saisandeep99@gmail.com>
2017-08-30 17:50:34 -0700

    Integration with JDBC

--- SpringSecSample/src/main/java/com/sai/sandeep/security/SecurityConfig.java
+++ SpringSecSample/src/main/java/com/sai/sandeep/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.sai.sandeep.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,14 +14,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
+
+		//In Memeory Authentication
+		/*		auth
 		.inMemoryAuthentication()
 		.withUser("user").password("password").roles("USER")
 		.and()
-		.withUser("admin").password("password").roles("USER","ADMIN");
+		.withUser("admin").password("password").roles("USER","ADMIN");*/
 		
-		
+		auth.jdbcAuthentication().dataSource(dataSource);
 	}
 	
 	public void configure(HttpSecurity http) throws Exception{
--- SpringSecSample/target/SpringSecSample-0.0.1-SNAPSHOT/index.jsp
+++ SpringSecSample/target/SpringSecSample-0.0.1-SNAPSHOT/index.jsp
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+
 <html>
 	<head>
 		<meta charset="utf-8">
 		<title>Welcome</title>
 	</head> 
 	<body>
-		
+		Username :<sec:authentication property="principal.username"/><br/>
 		<a href="admin">Click to Admin area</a>
 		<form action="Logout" method="post">
 		<input type="submit" value="logout">

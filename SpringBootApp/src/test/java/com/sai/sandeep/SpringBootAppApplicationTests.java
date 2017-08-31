730dce391e5e8fd4a03ab657877bd66b27aa3f50
Sai Sandeep Nandyala <saisandeep99@gmail.com>
2017-08-31 11:24:09 -0700

    JPA entities

--- SpringBootApp/src/test/java/com/sai/sandeep/SpringBootAppApplicationTests.java
+++ SpringBootApp/src/test/java/com/sai/sandeep/SpringBootAppApplicationTests.java
@@ -4,9 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.sai.sandeep.SpringBootAppApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SpringBootAppApplication.class)
+@WebAppConfiguration
 public class SpringBootAppApplicationTests {
 
 	@Test

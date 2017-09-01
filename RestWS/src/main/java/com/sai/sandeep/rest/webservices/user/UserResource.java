a98916d7ed167d5ca2653b69cd463bcd3273fc0b
Sai Sandeep Nandyala <saisandeep99@gmail.com>
2017-09-01 16:21:54 -0700

    Added Delete Functionality

--- RestWS/src/main/java/com/sai/sandeep/rest/webservices/user/UserResource.java
+++ RestWS/src/main/java/com/sai/sandeep/rest/webservices/user/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,21 +28,27 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retrieveOneUser(@PathVariable int id) {
 		User user = service.findOne(id);
-		if(user==null)
-			throw new UserNotFoundException("id - "+id);
+		if (user == null)
+			throw new UserNotFoundException("id - " + id);
 		return user;
 
 	}
 
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user){
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = service.save(user);
-		URI location = ServletUriComponentsBuilder
-		.fromCurrentRequest()
-		.path("/{id}")
-		.buildAndExpand(savedUser.getId())
-		.toUri(); 
-		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+
 		return ResponseEntity.created(location).build();
 	}
+
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.deleteById(id);
+		if (user == null)
+			throw new UserNotFoundException("id - " + id);
+
+	}
+
 }

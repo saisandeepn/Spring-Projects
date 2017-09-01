c6c32d8768285a05dea6cc5bf7e272f79d409a16
Sai Sandeep Nandyala <saisandeep99@gmail.com>
2017-08-31 21:39:05 -0700

    Added implementation to get all courses

--- soap-course-management/src/main/java/com/sai/sandeep/soapcoursemanagement/soap/CourseDetailsEndpoint.java
+++ soap-course-management/src/main/java/com/sai/sandeep/soapcoursemanagement/soap/CourseDetailsEndpoint.java
@@ -1,5 +1,7 @@
 package com.sai.sandeep.soapcoursemanagement.soap;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -9,6 +11,8 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 import com.sai.sandeep.soapcoursemanagement.soap.bean.Course;
 import com.sai.sandeep.soapcoursemanagement.soap.service.CourseDetailsService;
 import com.saisandeep.courses.CourseDetails;
+import com.saisandeep.courses.GetAllCourseDetailsRequest;
+import com.saisandeep.courses.GetAllCourseDetailsResponse;
 import com.saisandeep.courses.GetCourseDetailsRequest;
 import com.saisandeep.courses.GetCourseDetailsResponse;
 
@@ -17,21 +21,43 @@ public class CourseDetailsEndpoint {
 
 	@Autowired
 	CourseDetailsService service;
-	
+
 	@PayloadRoot(namespace = "http://saisandeep.com/courses", localPart = "GetCourseDetailsRequest")
 	@ResponsePayload
-	public GetCourseDetailsResponse processRequest(@RequestPayload GetCourseDetailsRequest request) {
+	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
 		Course course = service.findById(request.getId());
-		return mapCourse(course);
+		return mapCourseDetails(course);
 	}
 
-	private GetCourseDetailsResponse mapCourse(Course course) {
+	private GetCourseDetailsResponse mapCourseDetails(Course course) {
 		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		response.setCourseDetails(mapCourse(course));
+		return response;
+	}
+
+	private GetAllCourseDetailsResponse mapAllCourseDetails(List<Course> courses) {
+		GetAllCourseDetailsResponse response = new GetAllCourseDetailsResponse();
+		for (Course course : courses) {
+			CourseDetails mapCourse = mapCourse(course);
+			response.getCourseDetails().add(mapCourse);
+		}
+		return response;
+	}
+
+	private CourseDetails mapCourse(Course course) {
 		CourseDetails courseDetails = new CourseDetails();
 		courseDetails.setId(course.getId());
 		courseDetails.setName(course.getName());
 		courseDetails.setDescription(course.getDescription());
-		response.setCourseDetails(courseDetails);
-		return response;
+		return courseDetails;
 	}
+
+	@PayloadRoot(namespace = "http://saisandeep.com/courses", localPart = "GetAllCourseDetailsRequest")
+	@ResponsePayload
+	public GetAllCourseDetailsResponse processAllCourseDetailsRequest(
+			@RequestPayload GetAllCourseDetailsRequest request) {
+		List<Course> courses = service.findAll();
+		return mapAllCourseDetails(courses);
+	}
+
 }

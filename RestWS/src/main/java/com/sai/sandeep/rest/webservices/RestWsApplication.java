01fc4f2f7e368810f72bb559ce47dcd14c16c7f8
Sai Sandeep Nandyala <saisandeep99@gmail.com>
2017-09-01 17:24:58 -0700

    Internationalization

--- RestWS/src/main/java/com/sai/sandeep/rest/webservices/RestWsApplication.java
+++ RestWS/src/main/java/com/sai/sandeep/rest/webservices/RestWsApplication.java
@@ -1,12 +1,32 @@
 package com.sai.sandeep.rest.webservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestWsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestWsApplication.class, args);
+	}
+	@Bean
+	public LocaleResolver localeResolver(){
+		SessionLocaleResolver localResolver = new SessionLocaleResolver();
+		localResolver.setDefaultLocale(Locale.US);
+		return localResolver;
+		
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource(){
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 }
